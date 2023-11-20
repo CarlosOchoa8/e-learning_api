a876609341cc32e9c6fed43b2e652caa06c5f2cf
carlos-ochoa8 <carlosochoa@tae.capital>
2023-11-19 19:59:41 -0700

    Add: get multi, get detail and update endpoints, also schemas of files was reformatted

--- app/schemas/lesson_file.py
+++ app/schemas/lesson_file.py
@@ -1,18 +1,6 @@
-from typing import Any
-
-from pydantic import Field, BaseModel, validator, root_validator, ValidationError, model_validator
 import json
-
-
-class BaseModelValidateToJSON(BaseModel):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate_to_json
-
-    @classmethod
-    def validate_to_json(cls, value):
-        """Validate to json"""
-        return cls(**json.loads(value)) if isinstance(value, str) else value
+from typing import ClassVar
+from pydantic import Field, BaseModel, model_validator
 
 
 class LessonFileBaseSchema(BaseModel):
@@ -22,23 +10,18 @@ class LessonFileBaseSchema(BaseModel):
 
 
 class LessonFileCreateSchema(LessonFileBaseSchema):
-    # name: str | dict[str, Any] = Field(min_length=5)
-    # lesson_id: int | dict[str, Any]
-
-    # @root_validator(pre=True)
-    # def parse_json(cls, value):
-    #     return cls(**json.loads(value)) if isinstance(value, str) else value
     @model_validator(mode='before')
     def parse_json(cls, value):
         return cls(**json.loads(value)) if isinstance(value, str) else value
 
 
 class LessonFileUpdateSchema(LessonFileBaseSchema):
-    pass
+    lesson_id: ClassVar[int]
 
 
 class LessonFileInDBSchema(LessonFileBaseSchema):
     id: int
+    file: str
 
     class Config:
         """
@@ -48,4 +31,4 @@ class LessonFileInDBSchema(LessonFileBaseSchema):
 
 
 class LessonFilesInDBSchema(BaseModel):
-    courses: list[LessonFileInDBSchema]
+    files: list[LessonFileInDBSchema]
