b36f9fcc5e5b9c2d50b6ce1b4a062342930855d7
carlos-ochoa8 <carlosochoa@tae.capital>
2024-01-08 19:50:34 -0600

    add: Progress endpoints

--- app/endpoints/__init__.py
+++ app/endpoints/__init__.py
@@ -2,3 +2,4 @@ from .user import user_router
 from .course import course_router
 from .lesson import lesson_router
 from .lesson_file import lesson_file_router
+from .progress import progress_router
